0cf65556777f9e0002040bf32eb9e3cf29dcc515
CeciGaspar <cecigaspar15@icloud.com>
2022-09-07 13:38:44 -0300

    Semana4_GasparCecilia

--- src/containers/reto6.js
+++
@@ -1,9 +0,0 @@
-let cadena = "Hola Mundo"
-let alReves=[]
-let cantIndex = 0
-cantIndex = cadena.length
-while (cantIndex > 0){
-    alReves += cadena[cantIndex-1]
-    cantIndex = cantIndex - 1 
-    imprimir = alReves.toString()}
-console.log(imprimir)
